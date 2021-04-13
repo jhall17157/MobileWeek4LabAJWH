f2f10e6baee70ebbbd06a8b111325ef81876a100
jhall41 <jhall41@my.wctc.edu>
2021-04-12 19:54:08 -0600

    First

---
+++ MorseCode/MorseCode/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MorseCode
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private string code = "";
+        private string theWord = "";
+
+        private void DashClick(object sender, EventArgs e)
+        {
+            code += "-";
+            morseLabel.Text = code;
+        }
+        private void DotClick (object sender, EventArgs e)
+        {
+            code += ".";
+            morseLabel.Text = code;
+        }
+        private void SpaceClick(object sender, EventArgs e)
+        {
+            char newLetter = Morse.MorseCoder(code);
+            code = morseLabel.Text = "";
+            theWord += newLetter;
+            word.Text = theWord;
+
+        }
+      
+    }
+}
